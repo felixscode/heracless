5714a3cc25695fb07f97b0ed80f481e8a86126e6
Felix Schelling <felix.schelling@protonmail.com>
2024-12-13 19:49:51 +0100

    Add type annotations and dataclasses for configuration models

---
+++ tests/config/types.pyi
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+from datetime import datetime
+from datetime import date
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class ProductItem:
+    sku: str
+    quantity: int
+    description: str
+    price: float
+
+
+@dataclass(frozen=True)
+class ShipTo:
+    given: str
+    family: str
+    address: "Address"
+
+
+@dataclass(frozen=True)
+class BillTo:
+    given: str
+    family: str
+    address: "Address"
+
+
+@dataclass(frozen=True)
+class Config:
+    invoice: int
+    date: date
+    bill_to: "BillTo"
+    ship_to: "ShipTo"
+    product: tuple["ProductItem"]
+    tax: float
+    total: float
+    comments: str
+
+
+@dataclass(frozen=True)
+class Address:
+    lines: str
+    city: str
+    state: str
+    postal: int
+
+
+def load_config(config_path: str) -> Config: ...
